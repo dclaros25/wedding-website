3e596e439a82ee28a57f1f46d9b6a771292bd12d
dclaros25 <47799293+dclaros25@users.noreply.github.com>
2023-08-22 08:38:04 -0700

    maps

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 13.75235, lng: -89.26680};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
