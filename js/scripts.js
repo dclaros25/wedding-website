66fb348a6dbd9d41c48fc34c460bf4ebc4dcd089
dclaros25 <47799293+dclaros25@users.noreply.github.com>
2023-08-22 19:02:23 -0700

    minor fix

--- js/scripts.js
+++ js/scripts.js
@@ -212,11 +212,11 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Un segundo!</strong> Estamos guardando tus detalles.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Lo sentimos!</strong> Tu codigo de invitación es incorrecto.'));
         } else {
             $.post("https://script.google.com/macros/s/AKfycbyxpmRVFukkVp31PFMGSNxd43UYCiBOqVQw3V-i22zkNMY7INp4B06EPQwks6BAy6w/exec", data)
                 .done(function (data) {
@@ -230,7 +230,7 @@ $(document).ready(function () {
                 })
                 .fail(function (data) {
                     console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Lo sentimos!</strong> Tenemos problemas con el servidor.'));
                 });
         }
     });
