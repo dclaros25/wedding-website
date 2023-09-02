f6adc4f45c2a33b375d942366fa9846b62bcd68d
dclaros25 <47799293+dclaros25@users.noreply.github.com>
2023-09-02 11:44:43 -0700

    launch time

--- js/scripts.js
+++ js/scripts.js
@@ -136,6 +136,31 @@ $(document).ready(function () {
 
     });
 
+    /****************launch-time******************/
+    var countDownDate = new Date("March 30, 2024 15:00:00").getTime();
+    var x = setInterval(function(){
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        if(distance < 0){
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "00";
+            document.getElementById("hours").innerHTML = "00";
+            document.getElementById("minutes").innerHTML = "00";
+            document.getElementById("seconds").innerHTML = "00";
+        }
+    },1000);
+
     /********************** Social Share buttons ***********************/
     var share_bar = document.getElementsByClassName('share-bar');
     var po = document.createElement('script');
